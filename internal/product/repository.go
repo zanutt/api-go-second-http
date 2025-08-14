61f8f7077b9c15bc7d6ffbc466e446cd258a52d3
Matheus Zanutto Gaspar <matszg@live.com>
2025-08-14 17:56:58 -0300

    refactor: change the repository to InMemoryRepository at the moment

--- internal/product/repository.go
+++ internal/product/repository.go
@@ -5,43 +5,43 @@ import (
 	"go-marketplace/internal/models"
 )
 
-type ProductRepository struct {
+type InMemoryRepository struct {
 	products map[uint]models.Product
 	nextID   uint
 }
 
-func NewProductRepository() *ProductRepository {
+func NewInMemoryRepository() *InMemoryRepository {
 	products := map[uint]models.Product{
 		1: {ID: 1, Name: "Notebook", Price: 500000, Description: "Notebook gamer"},
 		2: {ID: 2, Name: "Mouse", Price: 15000, Description: "Notebook gamer"},
 	}
 
-	return &ProductRepository{
+	return &InMemoryRepository{
 		products: products,
 		nextID:   3,
 	}
 }
 
 // get all products
-func (r *ProductRepository) GetAll() []models.Product {
+func (r *InMemoryRepository) GetAll() ([]models.Product, error) {
 	var allProducts []models.Product
 	for _, product := range r.products {
 		allProducts = append(allProducts, product)
 	}
-	return allProducts
+	return allProducts, nil
 }
 
-func (r *ProductRepository) AddProduct(product models.Product) models.Product {
+func (r *InMemoryRepository) AddProduct(product models.Product) (models.Product, error) {
 	product.ID = r.nextID
 
 	r.products[product.ID] = product
 
 	r.nextID++
 
-	return product
+	return product, nil
 }
 
-func (r *ProductRepository) GetByID(id uint) (models.Product, error) {
+func (r *InMemoryRepository) GetByID(id uint) (models.Product, error) {
 	product, ok := r.products[id]
 	if !ok {
 		return models.Product{}, errors.New("product not found")
@@ -49,7 +49,7 @@ func (r *ProductRepository) GetByID(id uint) (models.Product, error) {
 	return product, nil
 }
 
-func (r *ProductRepository) UpdateProduct(id uint, updatedProduct models.Product) (models.Product, error) {
+func (r *InMemoryRepository) UpdateProduct(id uint, updatedProduct models.Product) (models.Product, error) {
 	if _, ok := r.products[id]; !ok {
 		return models.Product{}, errors.New("product not found")
 	}
@@ -60,7 +60,7 @@ func (r *ProductRepository) UpdateProduct(id uint, updatedProduct models.Product
 	return updatedProduct, nil
 }
 
-func (r *ProductRepository) DeleteProduct(id uint) error {
+func (r *InMemoryRepository) DeleteProduct(id uint) error {
 	if _, ok := r.products[id]; !ok {
 		return errors.New("product not found")
 	}
