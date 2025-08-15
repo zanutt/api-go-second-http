686ae8cfc7c0501ba16be21ede8ab7a13faad751
Matheus Zanutto Gaspar <matszg@live.com>
2025-08-15 17:39:39 -0300

    test: add test to the login

--- internal/user/handler_test.go
+++ internal/user/handler_test.go
@@ -38,3 +38,29 @@ func TestRegisterHandler(t *testing.T) {
 	assert.NotEmpty(t, rr.Body.String(), "O corpo da resposta não deve estar vazio")
 
 }
+
+func TestLoginHandler_Sucess(t *testing.T) {
+	userRepo := NewInMemoryRepository()
+
+	handler := NewHandler(userRepo)
+
+	registrationData := `{"email":"test@example.com","password":"password123"}`
+	reqregister, err := http.NewRequest("POST", "/register", bytes.NewReader([]byte(registrationData)))
+	assert.NoError(t, err)
+
+	rrRegister := httptest.NewRecorder()
+
+	handler.RegisterUserHandler(rrRegister, reqregister)
+	assert.Equal(t, http.StatusCreated, rrRegister.Code, "Expected status code 201 Created")
+
+	// Agora, faça o login
+	loginData := `{"email":"test@example.com","password":"password123"}`
+	reqlogin, err := http.NewRequest("POST", "/login", bytes.NewReader([]byte(loginData)))
+	assert.NoError(t, err)
+
+	rrLogin := httptest.NewRecorder()
+	handler.LoginUserHandler(rrLogin, reqlogin)
+
+	assert.Equal(t, http.StatusOK, rrLogin.Code, "Expected status code 200 OK")
+	assert.NotEmpty(t, rrLogin.Body.String(), "O corpo da resposta não deve estar vazio")
+}
