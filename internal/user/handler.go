9cd5e82dffcfc2192392756400eee1d41c4bba80
Matheus Zanutto Gaspar <matszg@live.com>
2025-08-15 17:40:04 -0300

    feat: implement the login Handler

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -4,9 +4,13 @@ import (
 	"encoding/json"
 	"go-marketplace/internal/models"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var jwtKey = []byte("your_secret_key")
 
 type Handler struct {
 	repo UserRepository
@@ -43,4 +47,42 @@ func (h *Handler) RegisterUserHandler(w http.ResponseWriter, r *http.Request) {
 
 	createdUser.Password = "" // Do not expose the password hash
 	json.NewEncoder(w).Encode(createdUser)
+}
+
+func (h *Handler) LoginUserHandler(w http.ResponseWriter, r *http.Request) {
+	var credentials struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&credentials); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	foundUser, err := h.repo.FindByEmail(credentials.Email)
+	if err != nil {
+		http.Error(w, "user not found", http.StatusUnauthorized)
+		return
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(credentials.Password)); err != nil {
+		http.Error(w, "invalid credentials", http.StatusUnauthorized)
+		return
+	}
+	// Generate JWT token
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &models.Claims{
+		Email: foundUser.Email,
+		Role:  foundUser.Role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		http.Error(w, "failed to generate token", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
 }
