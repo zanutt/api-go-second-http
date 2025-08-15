90a1f16df08bb16163a62d71b137b03210366d2a
Matheus Zanutto Gaspar <matszg@live.com>
2025-08-15 17:39:51 -0300

    feat: create Claims struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,16 @@
 package models
 
+import "github.com/golang-jwt/jwt/v5"
+
 type User struct {
 	ID       uint   `json:"id"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 	Role     string `json:"role"`
+}
+
+type Claims struct {
+	Email string `json:"email"`
+	Role  string `json:"role"`
+	jwt.RegisteredClaims
 }
